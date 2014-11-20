8d4e74eda1fa6d224b52e471f9268142f4d6d666
Izzbot <izzbot@mindspring.com>
2014-11-20 11:05:44 -0800

    final

--- js/app.js
+++ js/app.js
@@ -1,37 +1,22 @@
-// List of Seattle Traffic Cameras
-// http://data.seattle.gov/resource/65fc-btcc.json
-
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
-
-
+// Start when the DOM is ready
 $(document).ready(function() {
 
-    // Set up the map
+    // Set up the starting variables
     var mapElem = document.getElementById('map')
-    // Center on Seattle
     var center = {
         lat: 47.6,
         lng: -122.3
     }
+    var infoWindow = new google.maps.InfoWindow();
+    var cameras = [];
 
     // Show the map
     var map = new google.maps.Map(mapElem, {
         center: center,
         zoom: 12
-    })
-
-    // Build an infoWindow
-    var infoWindow = new google.maps.InfoWindow();
-
-    // Build an array for the markers
+    });
 
     // Get the JSON data and parse it
     $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
@@ -47,10 +32,22 @@ $(document).ready(function() {
                     map: map
                 });
 
+                // Add the label to the marker object
+                marker.cameralabel = camera.cameralabel.toLowerCase();
+
+                // add it to the cameras array
+                cameras.push(marker);
+
                 // add a listener for the InfoWindow
                 google.maps.event.addListener(marker, 'click', function() {
                        // Pan to the camera
                     map.panTo(marker.getPosition());
+
+                    // Bounce the marker when it's hovered over
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function() {
+                        marker.setAnimation(null);
+                    }, 1500);
 
                     // Set up the info for the infoWindow
                     var html = '<p>' + camera.cameralabel + ' - <a href="' + camera.videourl + '">video</a><br><img src="'
@@ -72,12 +69,28 @@ $(document).ready(function() {
 
     // Set up the search filter
     $('#search').bind('search keyup', function() {
+        $('#ajax-loader').show();
         var search_term = $(this).val().toLowerCase();
 
-        // Check each of the camera labels
-        $('')
+        // Check each of the cameras
+        for (var i = 0, len = cameras.length; i < len; i++) {
+            var camera = cameras[i];
+
+            // Set each camera to hide if it doesn't match
+            var matched = cameras[i].cameralabel.indexOf(search_term);
+            if (matched >= 0) {
+                cameras[i].setMap(map);
+            } else {
+                cameras[i].setMap(null);
+            }
+        }
+
+        $('#ajax-loader').fadeOut();
     });
 
-
+    // Hide the infoWindow when the map is clicked
+    google.maps.event.addListener(map, 'click', function() {
+       infoWindow.close();
+    });
 
 });
